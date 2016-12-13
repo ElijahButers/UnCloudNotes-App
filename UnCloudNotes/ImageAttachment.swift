960beb8312e2278afc4dc6b8aa03628c09a87dbd
ElijahButers <Elijahbuters@gmail.com>
2016-12-13 17:40:42 +0200

    Added a image, width, height, caption properties to ImageAttachment class and removed an image property from  the Attachment class.

--- UnCloudNotes/ImageAttachment.swift
+++ UnCloudNotes/ImageAttachment.swift
@@ -10,4 +10,9 @@ import UIKit
 import CoreData
 
 class ImageAttachment: Attachment {
+  
+  @NSManaged var image: UIImage?
+  @NSManaged var width: Float
+  @NSManaged var height: Float
+  @NSManaged var caption: String
 }
