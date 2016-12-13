3f6a358b7169e0aa941472392d64a9712bb058fd
ElijahButers <Elijahbuters@gmail.com>
2016-12-13 17:11:28 +0200

    Added an ImageAttachment file with UIKit and CoreData imports to the project.

---
+++ UnCloudNotes/ImageAttachment.swift
@@ -0,0 +1,13 @@
+//
+//  ImageAttachment.swift
+//  UnCloudNotes
+//
+//  Created by User on 12/13/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ImageAttachment: Attachment {
+}
