f851caa9725b6b0c9f11962e68bd9bb3d3442157
ElijahButers <Elijahbuters@gmail.com>
2016-12-15 23:32:53 +0200

    Added a AttachmentToImageAttachmentMigrationPolicyV3toV4 class to the project where it is a custom migration policy and is for transforming data from Attachments in model version 3 to ImageAttachments in model version 4.

---
+++ UnCloudNotes/AttachmentToImageAttachmentMigrationPolicyV3toV4.swift
@@ -0,0 +1,16 @@
+//
+//  AttachmentToImageAttachmentMigrationPolicyV3toV4.swift
+//  UnCloudNotes
+//
+//  Created by User on 12/15/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+let errorDomain = "Migration"
+
+class AttachmentToImageAttachmentMigrationPolicyV3toV4: NSEntityMigrationPolicy {
+  
+}
